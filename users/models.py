214767522ab091786560b86721ac7d21acf010f1
Cayo Nakasato <cayonakasato112@gmail.com>
2022-10-28 11:39:17 -0400

    feat: created projet and app, kenzierol and users

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+import uuid
+
+class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    email = models.CharField(max_length = 50)
+    password = models.CharField(max_length = 50)
