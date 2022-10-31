714da964b3afa3f4130d271a215fa7a47d4c76b8
lVladimirl <infinitdungeun@gmail.com>
2022-10-31 19:29:54 -0300

    feat:model user base properties

--- users/models.py
+++ users/models.py
@@ -6,3 +6,5 @@ class User(AbstractUser):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     email = models.CharField(max_length = 50)
     password = models.CharField(max_length = 50)
+    bio = models.TextField()
+    birthday = models.DateTimeField()
