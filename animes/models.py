c5086e1708d9be6fc64a5dc731fa34029314d011
Cayo Nakasato <cayonakasato112@gmail.com>
2022-11-01 09:40:46 -0400

    hotfix: makemigration to delete duration on animes table

--- animes/models.py
+++ animes/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Anime(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=127)
-    duration = models.TimeField()
     total_eps = models.PositiveIntegerField()
     
     release_date = models.DateField()
