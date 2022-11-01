ea4b65746049064807c3b8d3e32811a76f86ca7d
lVladimirl <infinitdungeun@gmail.com>
2022-11-01 14:50:41 -0300

    feat:crud-user

--- users/views.py
+++ users/views.py
@@ -3,6 +3,12 @@ from rest_framework import generics
 from users.models import User
 from users.serializers import RegisterUserSerializer
 
-class CreateUserView(generics.CreateAPIView):
+class CreateUserView(generics.CreateAPIView, generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterUserSerializer
+    
+class ListUpdateUSerView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterUserSerializer
+    lookup_field = "id"
+
