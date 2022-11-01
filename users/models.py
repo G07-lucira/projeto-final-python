97e39bb10209bd1685c2590e8eb608364d900c0d
Cayo Nakasato <cayonakasato112@gmail.com>
2022-11-01 09:55:34 -0400

    feat: created views to create user

--- users/models.py
+++ users/models.py
@@ -6,11 +6,12 @@ class User(AbstractUser):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     email = models.CharField(max_length = 50)
     password = models.CharField(max_length = 50)
-    bio = models.TextField()
-    birthday = models.DateTimeField()
+    bio = models.TextField(null=True)
+    birthday = models.DateTimeField(null=True)
 
     animes = models.ManyToManyField(
         "animes.Anime",
         through="users_animes.UserAnimes",
-        related_name="users"
+        related_name="users",
+        null=True
     )
