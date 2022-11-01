dba4f3aba716b52da194ce3fea79e2e75c829176
Cayo Nakasato <cayonakasato112@gmail.com>
2022-11-01 13:38:47 -0400

    feat: episode serializer, incomplete need animes part to be completed

--- episodes/views.py
+++ episodes/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from episodes.models import Episode
+from episodes.serializers import EpisodeDetailSerializer, RegisterEpisodeSerializer
+
+class CreateEpisodeView(generics.CreateAPIView):
+    queryset = Episode.objects.all()
+    serializer_class = RegisterEpisodeSerializer
+
+    lookup_url_kwarg = 'anime_id'
+
+class ListEpisodeDetailView(generics.RetrieveAPIView):
+    queryset = Episode.objects.all()
+    serializer_class = EpisodeDetailSerializer
+
