84736d72858590e3960d3b5610c2d8a61ad3c476
Igor Garcia <igorgg@gmail.com>
2022-11-03 12:17:40 -0300

    fix: anime model fields

--- animes/models.py
+++ animes/models.py
@@ -6,11 +6,11 @@ from django.db import models
 class Anime(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=127)
-    total_eps = models.PositiveIntegerField(max_length=20)
+    total_eps = models.PositiveIntegerField()
     anime_img = models.CharField(max_length=127)
     synopsis = models.TextField()
-    author = models.CharField(max_length=127, null=True)
-    release_date = models.DateField()
+    author = models.CharField(max_length=127)
+    release_date = models.PositiveIntegerField()
     is_finished = models.BooleanField()
 
     genres = models.ManyToManyField("genres.Genre", related_name="animes")
