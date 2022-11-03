1ff131cd49560c9ee80ce4ac90bc1a75fc19ce72
Cayo Nakasato <cayonakasato112@gmail.com>
2022-11-03 11:41:43 -0400

    hotfix: make migrations

--- animes/urls.py
+++ animes/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import AnimesView, CreateDB
+from .views import AnimesView
 
 urlpatterns = [
-    path("animes/createdb/", CreateDB.as_view()),
+    #path("animes/createdb/", CreateDB.as_view()),
     path("animes/", AnimesView.as_view()),
 ]
