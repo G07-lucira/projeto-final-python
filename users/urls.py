a0ccbb5644ae2d39d55e3fe9b2c07d8831184648
lVladimirl <infinitdungeun@gmail.com>
2022-11-03 11:39:03 -0300

    feat:user-permissions-and-fixes

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CreateUserView, ListUpdateUSerView
+from .views import CreateUserView, ListDetailUSerView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns=[
     path('accounts/', CreateUserView.as_view()),
-    path('accounts/<str:id>/', ListUpdateUSerView.as_view()),
+    path('accounts/<str:id>/', ListDetailUSerView.as_view()),
+    path("login/", obtain_auth_token)
 ]
