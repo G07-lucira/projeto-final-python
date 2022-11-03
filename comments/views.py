40693b56e8009cd34aa2e2e8d36db1dc194ac346
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2022-11-03 13:22:59 -0300

    feat: comment view and route done

--- comments/views.py
+++ comments/views.py
@@ -1,9 +1,33 @@
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
 
 from comments.models import Comment
-from comments.serializers import CommentSerializer
+from comments.serializers import CommentSerializer, CommentDetailSerializer
+
+from .mixins import SerializerByMethodMixin
 
 
-class CreateCommentView(generics.CreateAPIView):
+class ListCreateCommentView(
+    SerializerByMethodMixin,
+    generics.ListCreateAPIView,
+):
+    authentication_classes = [TokenAuthentication]
     queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
+    serializer_map = {
+        "GET": CommentDetailSerializer,
+        "POST": CommentSerializer,
+    }
+    lookup_url_kwarg = "episode_id"
+
+
+class RetrieveUpdateDestroyCommentView(
+    SerializerByMethodMixin,
+    generics.RetrieveUpdateDestroyAPIView,
+):
+    authentication_classes = [TokenAuthentication]
+    queryset = Comment.objects.all()
+    lookup_url_kwarg = "comment_id"
+    serializer_map = {
+        "GET": CommentDetailSerializer,
+        "PATCH": CommentSerializer,
+    }
