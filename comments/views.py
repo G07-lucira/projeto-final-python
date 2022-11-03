12809ab16ae78291f7ef0218679aaf09fb01b711
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2022-11-03 15:32:58 -0300

    refact: add permissions on views

--- comments/views.py
+++ comments/views.py
@@ -1,26 +1,38 @@
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 
+from django.shortcuts import get_object_or_404
+
 from comments.models import Comment
 from comments.serializers import CommentSerializer, CommentDetailSerializer
 
-from .mixins import SerializerByMethodMixin
+from utils.permissions import isAdminOrOwner
 
-class CreateCommentView(generics.CreateAPIView):
+from episodes.models import Episode
+
+
+class ListCreateCommentView(generics.ListCreateAPIView):
+    permission_classes = [isAdminOrOwner]
     authentication_classes = [TokenAuthentication]
+
     queryset = Comment.objects.all()
-    serializer_class = [CommentSerializer]
+    serializer_class = CommentSerializer
+
     lookup_url_kwarg = "episode_id"
 
+    def perform_create(self, serializer):
+        episode_id = self.kwargs["episode_id"]
 
-class RetrieveUpdateDestroyCommentView(
-    SerializerByMethodMixin,
-    generics.RetrieveUpdateDestroyAPIView,
-):
+        episode = get_object_or_404(Episode, pk=episode_id)
+
+        serializer.save(episode=episode)
+
+
+class RetrieveUpdateDestroyCommentView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [TokenAuthentication]
+    permission_classes = [isAdminOrOwner]
+
     queryset = Comment.objects.all()
+    serializer_class = CommentDetailSerializer
+
     lookup_url_kwarg = "comment_id"
-    serializer_map = {
-        "GET": CommentDetailSerializer,
-        "PATCH": CommentSerializer,
-    }
