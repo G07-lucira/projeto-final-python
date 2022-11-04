004fe10de9d841266a1ec77d11171e9bd98762f3
Cayo Nakasato <cayonakasato112@gmail.com>
2022-11-04 09:37:14 -0400

    hotfix: fixed files names

--- episodes/tests/test_models.py
+++ episodes/tests/test_models.py
@@ -1,39 +1,40 @@
 from django.test import TestCase
-
-from django.test import TestCase
-
 from model_bakery import baker
 
-from animes.models import Anime
-from genres.models import Genre
-from episodes.models import Episode
+import ipdb
 
-class GenreTestModel(TestCase):
+class AccountModelTest(TestCase):
+
     @classmethod
-    def setUp(self):
-        self.genre_jojo = baker.make(
-            Genre,
-            name="Jojo",
-        )
-        self.anime_x = baker.make(
-            Anime,
-            name = "Dungeon ni Deai o Motomeru no wa Machigatteiru Darou ka",
-            total_eps = 50,
-            anime_img = "https://animesbr.biz/wp-content/uploads/2019/07/hR4zYTRJZiSYooocDRPW7P3PCTF-185x278.jpg",
-            synopsis = "Baseado na light novel de mesmo nome escrita por Fujino Omori e ilustrada por Suzuhito Yasuda, Dungeon ni Deai o Motomeru no wa Machigatteiru Darou ka se passa no mundo de Orario, onde aventureiros se unem para caçar tesouros em labirintos subterrâneos conhecidos como Dungeon. No entanto, para Bell Cranel, fama e dinheiro estão em segundo plano; o que ele mais quer encontrar, na verdade, são garotas! Mas ele logo descobre que dentro de uma Dungeon tudo pode acontecer e, no fim, ele que acaba sendo a donzela em perigo!",
-            author = "Fujino Omori",
-            release_date = 6,
-            is_finished = True,
-            genres=[self.genre_jojo]
-        )
-        self.episode_69 = baker.make(
-            Episode,
-            name = "um macaco no navio",
-            epi_number = 69,
-            duration = "22:30",
-            anime= self.anime_x
-        )
-    def test_create_anime_episode(self):
-        episode = self.episode_69
-        self.assertIsInstance(episode, Episode)
+    def setUpTestData(cls):
+        cls.episode_create = baker.make_recipe('episodes.new_episode')
+        
+
+    def test_name_max_legth(self):
+
+        max_length = self.episode_create._meta.get_field('name').max_length
+
+        self.assertEqual(max_length, 127)
+
+    def test_duration_max_legth(self):
+
+        max_length = self.episode_create._meta.get_field('duration').max_length
+
+        self.assertEqual(max_length, 10)
+
+    def test_has_correct_fields(self):
+        self.assertEqual(self.episode_create.name, "Episode atulizado")
+        self.assertEqual(self.episode_create.epi_number, 1)
+        self.assertEqual(self.episode_create.duration, '18:00')
+    
+    def test_epi_number_is_positive(self):
+        self.assertTrue(self.episode_create.epi_number > 0)
+
+    def test_episode_has_anime_id(self):
+        self.assertTrue(self.episode_create.anime_id)
+
+    
+
+
+
 
