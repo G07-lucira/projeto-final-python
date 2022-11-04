9a889d3eeb1de6d61c0265859e0bb34a4e7060d2
Igor Garcia <igorgg@gmail.com>
2022-11-04 11:56:52 -0300

    hotfix: erase ipdb line

--- utils/permissions.py
+++ utils/permissions.py
@@ -6,7 +6,7 @@ class isAdminOrOwner(permissions.BasePermission):
     def has_object_permission(self, req, view, obj) -> bool:
         if req.method in permissions.SAFE_METHODS:
             return True
-        ipdb.set_trace()
+        
         return (
             req.user.is_authenticated
             and req.user.is_superuser
