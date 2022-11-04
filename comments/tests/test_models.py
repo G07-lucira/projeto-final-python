61e647badf8ae2c95bc831233af9a7cd1f736818
lVladimirl <infinitdungeun@gmail.com>
2022-11-04 11:33:22 -0300

    feat:test-models-all

--- comments/tests/test_models.py
+++ comments/tests/test_models.py
@@ -10,8 +10,15 @@ from comments.models import Comment
 class GenreTestModel(TestCase):
     @classmethod
     def setUp(self):
-        self.comment = baker.make(Comment)
-    def test_create_comment_episode(self):
-        comment = self.comment
-        self.assertIsInstance(comment, Comment)
+        self.episode = baker.make("episodes.Episode")
+        self.user = baker.make("users.User")
+        self.comment_data = {
+            "comment":"gostei muito, tem bolo",
+            "episode_id": self.episode.id,
+            "user_id": self.user.id
+        }
+        self.comment = Comment.objects.create(**self.comment_data)
+    def test_spoiler_is_default_false(self):
+        default_false = self.comment._meta.get_field('spoiler').default
 
+        self.assertEqual(default_false, False)
