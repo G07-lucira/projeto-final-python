f4db85d9ceb336b88b00c5134d63a63749eeefc1
lVladimirl <infinitdungeun@gmail.com>
2022-11-04 09:36:54 -0300

    feat:test-models-users

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,26 +1,16 @@
 from django.test import TestCase
 
 from model_bakery import baker
-
-from users.serializers import UserDetailSerializer
 from users.models import User
 
 class UserTestModel(TestCase):
     @classmethod
     def setUp(self):
-        self.user_joe = baker.make(
-            User,
-            email="raony.teste@gmail.com",
-            password="teste1",
-            username="raony",
-            bio="eu estou perdido",
-            birthday="2003-06-02",
-        )
+        self.user_joe = baker.make("users.User")
+        self.anime_x = baker.make("animes.Anime")
+        self.episode_y = baker.make("episodes.Episode")
+        self.comment_z= baker.make("comments.Comment")
 
     def test_create_user(self):
-        self.user_one = self.user_joe
-        serializer = UserDetailSerializer(self.user_one)
-        self.assertIsInstance(self.user_one, User)
-        self.assertEqual(serializer.data["username"], "raony")
-        self.assertEqual(serializer.data["bio"], "eu estou perdido")
-        self.assertEqual(serializer.data["birthday"], "2003-06-02")
+        user_one = self.user_joe
+        self.assertIsInstance(user_one, User)
