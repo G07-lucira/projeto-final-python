f3f9c8794d6bcdfa55e51b434ad216945f6978d6
Igor Garcia <igorgg@gmail.com>
2022-11-04 11:54:54 -0300

    hotfix: permission fixed for owner

--- utils/permissions.py
+++ utils/permissions.py
@@ -6,10 +6,10 @@ class isAdminOrOwner(permissions.BasePermission):
     def has_object_permission(self, req, view, obj) -> bool:
         if req.method in permissions.SAFE_METHODS:
             return True
-
+        ipdb.set_trace()
         return (
             req.user.is_authenticated
             and req.user.is_superuser
             or req.user.is_authenticated
-            and obj.user.id == req.user.id
+            and obj.id == req.user.id
         )
