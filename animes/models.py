5cbb5e6e76770641e53cee68a501341c438b0f3e
Igor Garcia <igorgg@gmail.com>
2022-11-07 11:58:26 -0300

    fix: anime total eps

--- animes/models.py
+++ animes/models.py
@@ -6,7 +6,6 @@ from django.db import models
 class Anime(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=127)
-    total_eps = models.PositiveIntegerField()
     anime_img = models.CharField(max_length=127)
     synopsis = models.TextField()
     author = models.CharField(max_length=127)
@@ -14,3 +13,4 @@ class Anime(models.Model):
     is_finished = models.BooleanField()
 
     genres = models.ManyToManyField("genres.Genre", related_name="animes")
+    
